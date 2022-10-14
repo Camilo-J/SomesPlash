e3ee11d826f7de1568c375ba4377476ec25b2dff
Elías Mesones <eliasmesones@gmail.com>
2022-10-14 10:33:45 -0500

    ADD: Validations to image, comment and category

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true, counter_cache: true
+  validates: body, presence: true
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,9 @@
 class Image < ApplicationRecord
+  # Associations
   has_one_attached :photo
   belongs_to :category, counter_cache: true
   has_many :comments, as: :commentable, dependent: :destroy
+  # Validations
+  validates: title, presence: true
+  validates: category_id, presence: true
 end
