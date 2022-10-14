48bee898ecfa488a30387d63961313918ff3f48a
Camilo-J <josecamilo1902@outlook.com>
2022-10-13 22:56:45 -0500

    Change names of migration that add a column to Category & Images

--- db/migrate/20221013230421_add_column_photo_count_to_category.rb
+++ db/migrate/20221013230421_add_column_photo_count_to_category.rb
@@ -1,5 +1,5 @@
 class AddColumnPhotoCountToCategory < ActiveRecord::Migration[7.0]
   def change
-    add_column :categories, :photo_count, :integer, default: 0
+    add_column :categories, :photos_count, :integer, default: 0
   end
 end
--- db/migrate/20221013230921_add_column_comment_count_to_image.rb
+++ db/migrate/20221013230921_add_column_comment_count_to_image.rb
@@ -1,5 +1,5 @@
 class AddColumnCommentCountToImage < ActiveRecord::Migration[7.0]
   def change
-    add_column :images, :comment_count, :integer, default: 0
+    add_column :images, :comments_count, :integer, default: 0
   end
 end
