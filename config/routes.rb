1b39751578feba41fcdd1200169ca8cc3c1c2c60
Camilo-J <josecamilo1902@outlook.com>
2022-10-14 16:35:10 -0500

    Add image controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "categories#index"
-  
+
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :images
   # Defines the root path route ("/")
   # root "articles#index"
 end
