983b72a8fbe168297b29baf9da8b84b1a3cf0cc1
minanodiego <minanodiego5@gmail.com>
2022-10-14 12:14:20 -0500

    add: categories controller and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get "categories/index"
-  get "categories/new"
-  get "categories/show"
-  get "categories/create"
-  get "categories/edit"
-  get "categories/update"
-  get "categories/destroy"
+  root "categories#index"
+  
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
