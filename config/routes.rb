ba39c1368fc428ad2c28e1da45eb66bc3f8e914b
Camilo-J <josecamilo1902@outlook.com>
2022-10-15 13:03:48 -0500

    Fix some bugs founded in category's views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'index/images'
-  get 'index/new'
-  get 'index/show'
-  get 'index/create'
-  get 'index/edit'
-  get 'index/update'
-  get 'index/destroy'
-  root "categories#index"
 
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
