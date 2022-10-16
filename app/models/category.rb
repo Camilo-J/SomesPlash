6f9b4b4e0df8b46dc178e5084068a687a57bab58
Camilo-J <josecamilo1902@outlook.com>
2022-10-15 22:04:14 -0500

    Add  validation to destroy relations in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   # Associations
   has_one_attached :photo
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :images
+  has_many :images, dependent: :destroy
   # Validations
   validates :name, presence: true
 end
